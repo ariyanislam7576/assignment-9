e490536722e105cff225f225dbfb39c583e8c1e3
unknown <ma5665711@gmail.com>
2021-10-05 06:11:36 -0700

    update

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -5,17 +5,28 @@ const Footer = () => {
     return (
         // footer component
          
-        <div className='footer'>
-            <footer>
-                <div className='input'>
+        <div className='row d-flex'>
+            <div className="col-md-6 text-start m-5">
+                <h2>ITHome</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum cum laudantium distinctio?
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt magni repudiandae, a nihil obcaecati quaerat beatae ratione distinctio dolores omnis nam quos, dolore quibusdam possimus doloremque expedita consectetur, amet quae!
+                </p>
+            </div>
+            <div className="col-md-6 text-end">
+            <div className='input'>
                     <h3>Contact us</h3>
                     <form>
                         <input type="text" placeholder='Enter your name' /><br />
                         <input type="email" name="" id="" placeholder='Enter Your E-mail' /><br />
-                        <textarea name="" id="" cols="25" rows="6" placeholder='say somethig about our service'></textarea><br />
-                        <button className='btn-danger p-2 rounded' type="submit">Submit</button>
+
+                        <textarea name="" id="" cols="25" rows="6" placeholder='say somethig about our service'>
+                        </textarea><br />
+                        <button className='btn-danger p-2 rounded' type="submit" >Submit</button>
                     </form>
                 </div>
+            </div>
+            <footer>
+                
                 
                 <p>All right reserved by ITHome</p>
             </footer>
